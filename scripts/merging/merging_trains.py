aba9f43cfb3a23dc196861055d88b40fc02a7a68
Sacha-lma <sacha.le-moign-avalos@epitech.eu>
2026-05-20 12:21:44 +0200

    feat: update merge_trains function to return complete DataFrame with additional station data

--- scripts/merging/merging_trains.py
+++ scripts/merging/merging_trains.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def merge_trains(delay_df: pd.DataFrame, station_df: pd.DataFrame) -> tuple[pd.DataFrame]:
+
+def merge_trains(delay_df: pd.DataFrame, station_df: pd.DataFrame) -> pd.DataFrame:
     delay_df = delay_df.merge(
         station_df.rename(
             columns={
@@ -8,22 +9,47 @@ def merge_trains(delay_df: pd.DataFrame, station_df: pd.DataFrame) -> tuple[pd.D
                 "Trigramme": "departure_station_trigrame",
                 "Position géographique": "departure_station_geo",
                 "Segment(s) DRG": "departure_station_segment",
-                "Code commune": "",
-                "Code_UIC": "",
-                "Id_Gare": 
+                "Code commune": "departure_station_comunal_code",
+                "Code_UIC": "departure_station_UIC_code",
+                "Id_Gare": "departure_station_id",
             }
-        )[["Departure station", "departure_station_id", "departure_station_geo"]],
+        )[
+            [
+                "Departure station",
+                "departure_station_id",
+                "departure_station_geo",
+                "departure_station_trigrame",
+                "departure_station_segment",
+                "departure_station_comunal_code",
+                "departure_station_UIC_code",
+            ]
+        ],
         on="Departure station",
         how="left",
     )
     delay_df = delay_df.merge(
         station_df.rename(
             columns={
-                "station_name": "Arrival station",
-                "station_id": "arrival_station_id",
-                "geo_position": "arrival_station_geo",
+                "Nom_Gare": "Arrival station",
+                "Trigramme": "arrival_station_trigrame",
+                "Position géographique": "arrival_station_geo",
+                "Segment(s) DRG": "arrival_station_segment",
+                "Code commune": "arrival_station_comunal_code",
+                "Code_UIC": "arrival_station_UIC_code",
+                "Id_Gare": "arrival_station_id",
             }
-        )[["Arrival station", "arrival_station_id", "arrival_station_geo"]],
+        )[
+            [
+                "Arrival station",
+                "arrival_station_id",
+                "arrival_station_geo",
+                "arrival_station_trigrame",
+                "arrival_station_segment",
+                "arrival_station_comunal_code",
+                "arrival_station_UIC_code",
+            ]
+        ],
         on="Arrival station",
         how="left",
     )
+    return delay_df
